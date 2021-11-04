d20929973ac66f31f0a0812be14a095d1b3941bd
wallace <wallacegibbon@outlook.com>
2021-11-04 16:19:29 +0800

    improvement(cPreprocessor): the newlines after instructions like #else, #elif, #endif are not necessary, and related codes got removed.

--- src/cPreprocessor.erl
+++ src/cPreprocessor.erl
@@ -21,9 +21,9 @@ handleSpecial([{identifier, _, define}, {identifier, LineNumber, Name} | Rest], 
             {Tokens, RestTokens} = getExpressionTillEOL(Rest),
             handleNormal(RestTokens, {MacroMap#{Name => Tokens}, TokensToReturn, EndTag})
     end;
-handleSpecial([{identifier, _, undef}, {identifier, _, Name}, {newline, _} | Rest], {MacroMap, TokensToReturn, EndTag}) ->
+handleSpecial([{identifier, _, undef}, {identifier, _, Name} | Rest], {MacroMap, TokensToReturn, EndTag}) ->
     handleNormal(Rest, {maps:remove(Name, MacroMap), TokensToReturn, EndTag});
-handleSpecial([{identifier, _, ifdef}, {identifier, _, Name}, {newline, _} | Rest], {MacroMap, _, EndTag} = Context) ->
+handleSpecial([{identifier, _, ifdef}, {identifier, _, Name} | Rest], {MacroMap, _, EndTag} = Context) ->
     {MacroMapNew, CollectedTokens, RestTokensNew} = case MacroMap of
                                                         #{Name := _} ->
                                                             collectToElseAndIgnoreToEndif(Rest, Context);
@@ -33,7 +33,7 @@ handleSpecial([{identifier, _, ifdef}, {identifier, _, Name}, {newline, _} | Res
     handleNormal(RestTokensNew, {MacroMapNew, CollectedTokens, EndTag});
 handleSpecial([{identifier, LineNumber, ifdef} | _], _) ->
     throw({LineNumber, "invalid #ifdef command"});
-handleSpecial([{identifier, _, ifndef}, {identifier, _, Name}, {newline, _} | Rest], {MacroMap, _, EndTag} = Context) ->
+handleSpecial([{identifier, _, ifndef}, {identifier, _, Name} | Rest], {MacroMap, _, EndTag} = Context) ->
     {MacroMapNew, CollectedTokens, RestTokensNew} = case MacroMap of
                                                         #{Name := _} ->
                                                             ignoreToElseAndCollectToEndif(Rest, Context);
@@ -52,28 +52,23 @@ handleSpecial([{identifier, _, 'if'} | Rest], {MacroMap, _, EndTag} = Context) -
                                                             ignoreToElseAndCollectToEndif(RestTokens, Context)
                                                     end,
     handleNormal(RestTokensNew, {MacroMapNew, CollectedTokens, EndTag});
-handleSpecial([{identifier, _, else}, {newline, _} | RestContent], {MacroMap, TokensToReturn, else}) ->
+handleSpecial([{identifier, _, else} | RestContent], {MacroMap, TokensToReturn, else}) ->
     {MacroMap, TokensToReturn, RestContent};
-handleSpecial([{identifier, LineNumber, else}, {newline, _} | _], {_, _, normal}) ->
+handleSpecial([{identifier, LineNumber, else} | _], {_, _, normal}) ->
     throw({LineNumber, "\"#else\" is not expected here"});
-handleSpecial([{identifier, LineNumber, else} | _], _) ->
-    throw({LineNumber, "syntax error on \"#else\""});
-handleSpecial([{identifier, _, endif}, {newline, _} | RestContent], {MacroMap, TokensToReturn, endif}) ->
+handleSpecial([{identifier, _, endif} | RestContent], {MacroMap, TokensToReturn, endif}) ->
     {MacroMap, TokensToReturn, RestContent};
 %% when the "#else" part is missing ("#if" following "#endif"), pretend that the "#else\n" exists and has been swallowed,
 %% and put the "#endif" back to unhandled tokens.
-handleSpecial([{identifier, LineNumber, endif}, {newline, _} | _] = Content, {MacroMap, TokensToReturn, else}) ->
+handleSpecial([{identifier, LineNumber, endif} | _] = Content, {MacroMap, TokensToReturn, else}) ->
     {MacroMap, TokensToReturn, [{'#', LineNumber} | Content]};
-handleSpecial([{identifier, LineNumber, endif}] = Content, {MacroMap, TokensToReturn, else}) ->
-    {MacroMap, TokensToReturn, [{'#', LineNumber} | Content]};
-handleSpecial([{identifier, LineNumber, endif}, {newline, _} | _], {_, _, normal}) ->
-    throw({LineNumber, "\"#endif\" is not expected here"});
-handleSpecial([{identifier, LineNumber, endif} | _], _) ->
-    throw({LineNumber, "syntax error on \"#endif\""});
 handleSpecial([{identifier, LineNumber, error} | _], _) ->
     throw({LineNumber, "compile error... (todo)"});
+handleSpecial([{identifier, LineNumber, warning} | _], _) ->
+    throw({LineNumber, "compile warning... (todo)"});
 handleSpecial([{identifier, _, include} | Rest], Context) ->
-    handleNormal(Rest, Context);
+    {_, RestTokens} = getExpressionTillEOL(Rest),
+    handleNormal(RestTokens, Context);
 handleSpecial([{identifier, LineNumber, Name} | _], _) ->
     throw({LineNumber, cToolUtil:flatFmt("unexpected operator \"~s\" here", [Name])});
 handleSpecial([], {MacroMap, TokensToReturn, normal}) ->
@@ -121,6 +116,9 @@ getExpressionTillEOL(Tokens) ->
 -spec getExpressionTillEOL([token()], [token()]) -> {[token()], [token()]}.
 getExpressionTillEOL([{newline, _} | Rest], CollectedTokens) ->
     {lists:reverse(CollectedTokens), Rest};
+%% EOF should not appear in this function, but leave the error handling to upper level.
+getExpressionTillEOL([], CollectedTokens) ->
+    {lists:reverse(CollectedTokens), []};
 getExpressionTillEOL([Token | Rest], CollectedTokens) ->
     getExpressionTillEOL(Rest, [Token | CollectedTokens]).
 
@@ -194,14 +192,13 @@ process_elif_4_test() ->
 convertElifToElseAndIf(Tokens) ->
     lists:reverse(lists:flatten(convertElifToElseAndIf(Tokens, [], 0))).
 
-%% after this process, all "#endif" will have a newline after it.
 -spec convertElifToElseAndIf([token()], TokenTree, integer()) -> TokenTree when TokenTree :: [token() | TokenTree].
 convertElifToElseAndIf([{'#', _} = PreTag, {identifier, _, 'if'} = Token | Rest], CollectedTokens, _) ->
     [convertElifToElseAndIf(Rest, [], 0), Token, PreTag, CollectedTokens];
 convertElifToElseAndIf([{'#', _}, {identifier, LineNumber, elif} | Rest], CollectedTokens, ElifDepth) ->
     convertElifToElseAndIf(Rest, [makeElifReplacement(LineNumber) | CollectedTokens], ElifDepth + 1);
-convertElifToElseAndIf([{'#', _} = PreTag, {identifier, LineNumber, endif} = Token | Rest], CollectedTokens, ElifDepth) ->
-    convertElifToElseAndIf(Rest, [lists:duplicate(ElifDepth + 1, [{newline, LineNumber}, Token, PreTag]) | CollectedTokens], 0);
+convertElifToElseAndIf([{'#', _} = PreTag, {identifier, _, endif} = Token | Rest], CollectedTokens, ElifDepth) ->
+    convertElifToElseAndIf(Rest, [lists:duplicate(ElifDepth + 1, [Token, PreTag]) | CollectedTokens], 0);
 convertElifToElseAndIf([Token | Rest], CollectedTokens, ElifDepth) ->
     convertElifToElseAndIf(Rest, [Token | CollectedTokens], ElifDepth);
 convertElifToElseAndIf([], _, N) when N =/= 0 ->
@@ -211,28 +208,28 @@ convertElifToElseAndIf([], CollectedTokens, 0) ->
 
 -spec makeElifReplacement(integer()) -> [token()].
 makeElifReplacement(LineNumber) ->
-    lists:reverse([{'#', LineNumber}, {identifier, LineNumber, else}, {newline, LineNumber}, {'#', LineNumber}, {identifier, LineNumber, 'if'}]).
+    lists:reverse([{'#', LineNumber}, {identifier, LineNumber, else}, {'#', LineNumber}, {identifier, LineNumber, 'if'}]).
 
 -ifdef(EUNIT).
 
 convertElifToElseAndIf_1_test() ->
     {ok, Tokens} = cScanner:tokenize(<<"#if 1\n a\n #elif 1\n b\n #endif">>),
-    ?assertEqual(<<"# if 1 \n a \n # else \n # if 1 \n b \n # endif \n # endif \n">>,
+    ?assertEqual(<<"# if 1 \n a \n # else # if 1 \n b \n # endif # endif">>,
                  cScanner:tokensToBinaryString(convertElifToElseAndIf(Tokens))).
 
 convertElifToElseAndIf_2_test() ->
     {ok, Tokens} = cScanner:tokenize(<<"#if 0\n a\n #elif 0\n b\n #elif 1\n c\n #else\n d\n #endif">>),
-    ?assertEqual(<<"# if 0 \n a \n # else \n # if 0 \n b \n # else \n # if 1 \n c \n # else \n d \n # endif \n # endif \n # endif \n">>,
+    ?assertEqual(<<"# if 0 \n a \n # else # if 0 \n b \n # else # if 1 \n c \n # else \n d \n # endif # endif # endif">>,
                  cScanner:tokensToBinaryString(convertElifToElseAndIf(Tokens))).
 
 convertElifToElseAndIf_3_test() ->
     {ok, Tokens} = cScanner:tokenize(<<"#if 0\n #else\n #endif">>),
-    ?assertEqual(<<"# if 0 \n # else \n # endif \n">>,
+    ?assertEqual(<<"# if 0 \n # else \n # endif">>,
                  cScanner:tokensToBinaryString(convertElifToElseAndIf(Tokens))).
 
 convertElifToElseAndIf_4_test() ->
     {ok, Tokens} = cScanner:tokenize(<<"#if 0\n #if 1\n #else\n #endif #endif">>),
-    ?assertEqual(<<"# if 0 \n # if 1 \n # else \n # endif \n # endif \n">>,
+    ?assertEqual(<<"# if 0 \n # if 1 \n # else \n # endif # endif">>,
                  cScanner:tokensToBinaryString(convertElifToElseAndIf(Tokens))).
 
 -endif.
